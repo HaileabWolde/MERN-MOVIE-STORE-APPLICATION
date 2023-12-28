fb1efa7c42d1fb05ef368e7d139874d52b5efd3e
Haileab <womahaileab1428@gmail.com>
2023-12-28 11:58:19 +0300

    api

--- Movie Store application/server/index.js
+++ Movie Store application/server/index.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 8000;
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-
+app.get('/', (req, res)=>{
+    res.json('Hello')
+})
 app.use('/movies', movieRoutes)
 app.use('/users', userRoutes)
 
